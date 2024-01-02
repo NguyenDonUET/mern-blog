f992a70d96537065739a56fe62ab496b5f3ed845
NguyenDonUET <nguyendoncb@gmail.com>
2024-01-02 16:21:43 +0700

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import {
+  About,
+  Dashboard,
+  Home,
+  NotFound,
+  Projects,
+  SignIn,
+  SignUp,
+} from './pages'
+
 export default function App() {
-  return <div> App</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
---
+++ client/src/pages/index.jsx
@@ -0,0 +1,8 @@
+export { default as Home } from './Home'
+export { default as About } from './About'
+export { default as Dashboard } from './Dashboard'
+export { default as Projects } from './Projects'
+
+export { default as SignIn } from './SignIn'
+export { default as SignUp } from './SignUp'
+export { default as NotFound } from './NotFound'
