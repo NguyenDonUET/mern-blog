adf51a4999edca9d10122b170f0fa99fb46868db
NguyenDonUET <nguyendoncb@gmail.com>
2024-01-02 20:33:13 +0700

    connect to the database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 import express from 'express'
+import { connectDb } from './database/connectDb.js'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
 
 const PORT = 5000
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+
+const startServer = async () => {
+  await connectDb(process.env.MONGO_URI)
+  console.log('🔥 DB connected')
+  app.listen(PORT, () => {
+    console.log(`💖 Server is running on port ${PORT}`)
+  })
+}
+
+startServer()
