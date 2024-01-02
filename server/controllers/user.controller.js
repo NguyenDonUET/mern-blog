7e35ae2d7b4bc41cefa4f55a71ad0b64a77cda2c
NguyenDonUET <nguyendoncb@gmail.com>
2024-01-02 20:44:23 +0700

    create a test api route

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,5 @@
+const test = (req, res) => {
+  res.json({ msg: 'ok 2' })
+}
+
+export { test }
--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ import express from 'express'
 import { connectDb } from './database/connectDb.js'
 import dotenv from 'dotenv'
 dotenv.config()
-
+import userRoutes from './routes/user.route.js'
 const app = express()
+
+// routes
+app.use('/api/user', userRoutes)
 
 const PORT = 5000
 
